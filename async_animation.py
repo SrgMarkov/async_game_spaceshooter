10351dac41e6effaba34c94c0a7506b2c86b83e3
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-08-23 14:17:13 +0400

    Add offset_tics arg to blink function Add coroutines list Interate coroutine in coroutines.copy() Add frames cycle

--- async_animation.py
+++ async_animation.py
@@ -1,6 +1,7 @@
 import asyncio
 import curses
 import time
+from itertools import cycle
 from random import randint, choice
 from curses_tools import draw_frame, read_controls, get_frame_size
 
@@ -10,21 +11,21 @@ STARS_SYMBOLS = ['+', '*', '.', ':']
 STARS_COUNT = 300
 
 
-async def blink(canvas, row, column, symbol='*'):
+async def blink(canvas, row, column, offset_tics, symbol='*'):
     while True:
-        for tic in range(randint(1, 20)):
+        for tic in range(offset_tics):
             canvas.addstr(row, column, symbol, curses.A_DIM)
             await asyncio.sleep(0)
 
-        for tic in range(randint(1, 3)):
+        for tic in range(offset_tics):
             canvas.addstr(row, column, symbol)
             await asyncio.sleep(0)
 
-        for tic in range(randint(1, 5)):
+        for tic in range(offset_tics):
             canvas.addstr(row, column, symbol, curses.A_BOLD)
             await asyncio.sleep(0)
 
-        for tic in range(randint(1, 3)):
+        for tic in range(offset_tics):
             canvas.addstr(row, column, symbol)
             await asyncio.sleep(0)
 
@@ -68,26 +69,22 @@ def get_moving(route, move, ship_size, max_coord):
         return max_coord - ship_size
 
 
-async def animate_spaceship(canvas, row, column, max_coords):
+async def animate_spaceship(canvas, row, column, max_row, max_column):
     with open('animation/rocket_frame_1.txt', 'r') as rocket_frame_1:
-        frames = [rocket_frame_1.read()]
+        frame_1 = rocket_frame_1.read()
     with open('animation/rocket_frame_2.txt', 'r') as rocket_frame_2:
-        frames.append(rocket_frame_2.read())
+        frame_2 = rocket_frame_2.read()
 
-    max_row, max_column = max_coords[0], max_coords[1]
-    ship_size_row, ship_size_column = get_frame_size(frames[0])
+    ship_size_row, ship_size_column = get_frame_size(frame_1)
+    frames = [[frame_1, frame_2], [frame_2, frame_1]]
 
-    while True:
+    for frame in cycle(frames):
         row_move, column_move, space_press = read_controls(canvas)
 
-        draw_frame(canvas, row, column, frames[1], negative=True)
+        draw_frame(canvas, row, column, frame[0], negative=True)
         row = get_moving(row, row_move, ship_size_row, max_row)
         column = get_moving(column, column_move, ship_size_column, max_column)
-        draw_frame(canvas, row, column, frames[0])
-        await asyncio.sleep(0)
-
-        draw_frame(canvas, row, column, frames[0], negative=True)
-        draw_frame(canvas, row, column, frames[1])
+        draw_frame(canvas, row, column, frame[1])
         await asyncio.sleep(0)
 
 
@@ -97,24 +94,25 @@ def draw(canvas):
     max_coords = canvas.getmaxyx()
     max_row, max_column = max_coords[0] - 1, max_coords[1] - 1
     fire_coroutine = fire(canvas, int(max_row), int(max_column / 2))
-    coroutines = [fire_coroutine]
 
     spaceship_start_row = int(max_row / 2)
     spaceship_start_column = int(max_column / 2)
-    spaceship_coroutine = animate_spaceship(canvas, spaceship_start_row, spaceship_start_column, max_coords)
+    spaceship_coroutine = animate_spaceship(canvas, spaceship_start_row, spaceship_start_column, max_row, max_column)
+
+    coroutines = [fire_coroutine, spaceship_coroutine]
 
     for star in range(STARS_COUNT):
         star_symbol = choice(STARS_SYMBOLS)
-        coroutine = blink(canvas, randint(1, max_row), randint(1, max_column), star_symbol)
+        star_blink = randint(1, 20)
+        coroutine = blink(canvas, randint(1, max_row), randint(1, max_column), star_blink, star_symbol)
         coroutines.append(coroutine)
 
     while True:
-        for coroutine in coroutines:
+        for coroutine in coroutines.copy():
             try:
                 coroutine.send(None)
             except StopIteration:
                 coroutines.remove(coroutine)
-        spaceship_coroutine.send(None)
         canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
